3e2279a713459bf7e67e7392966c6828732d6a0a
CharlesCooper-K <charlescooper669@gmail.com>
2026-09-09 19:10:43 -0400

    Add comments to SimpleCalculator for clarity
    Added comments to improve code readability.

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -4,20 +4,25 @@ import java.util.Scanner;
 
 public class SimpleCalculator {
 	public static void main(String[] args) {
+		// declare and initialize variables
 		Scanner input = new Scanner(System.in);
 		int num1, num2;
 		String operation;
 		int value = 0;
-		
+
+		// ask user for first value
 		System.out.print("Enter first numbeer: ");
 		num1 = input.nextInt();
-		
+
+		// ask user for operation
 		System.out.print("Enter operation: ");
 		operation = input.next();
-		
+
+		// ask user for second value
 		System.out.print("Enter second number: ");
 		num2 = input.nextInt();
-		
+
+		// switch to do equation based on operation needed
 		switch (operation) {
 		case "+":
 			value = num1 + num2;
@@ -35,7 +40,8 @@ public class SimpleCalculator {
 			value = num1 / num2;
 			break;
 		}
-		
+
+		// print results
 		System.out.printf("%d %s %d = %d", num1, operation, num2, value);
 	}
 
